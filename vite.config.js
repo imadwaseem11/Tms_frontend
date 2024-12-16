03305409d0a7766448bb3e7210fa5f77305831bf
imadwaseem11 <sfixnow@gmail.com>
2024-12-16 13:41:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://tms-backend-tyfd.onrender.com",
         changeOrigin: true,
       },
     },
